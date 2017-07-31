1417404ce56e5ad3c82795723e975c7763766c34
Jordan Hudgens <jordan@devcamp.com>
2017-07-31 18:03:56 +0000

    updated fixe for show page image and added edit button

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -19,7 +19,6 @@ class PortfoliosController < ApplicationController
 
   def new
     @portfolio_item = Portfolio.new
-    3.times { @portfolio_item.technologies.build }
   end
 
   def create
@@ -77,7 +76,7 @@ end
                                       :body,
                                       :main_image,
                                       :thumb_image,
-                                      technologies_attributes: [:name]
+                                      technologies_attributes: [:id, :name, :_destroy]
                                      )
   end
 end
